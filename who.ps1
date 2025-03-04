68e5d5ab995fb26e7d331cc32cc15a45f0f876b9
venkatasai-redefine <vspm@redefine.co>
2025-03-04 13:05:53 +0530

    Update and rename who.ps1 to win_who.yml

--- who.ps1
+++
@@ -1,6 +0,0 @@
-$loggedInUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
-if ($loggedInUser) {
-    Write-Host "Current logged-in user: $loggedInUser"
-} else {
-    Write-Host "No user is currently logged in."
-}
